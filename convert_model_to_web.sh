be6a0461d94c2580b55924b3bec8519d5f7fe5da
micahcarroll <micah.d.carroll@gmail.com>
2019-08-07 15:40:21 -0700

    Added web models for all maps and types

--- convert_model_to_web.sh
+++ convert_model_to_web.sh
@@ -3,13 +3,21 @@ RUN_TYPE="$1"
 RUN_NAME="$2"
 SEED="$3"
 if [ "$#" -eq 3 ]
+then
+    if [ "$1" == "ppo_runs" ]
     then
-    tensorflowjs_converter --input_format=tf_saved_model --output_node_names='ppo_agent/ppo2_model/action_probs' --saved_model_tags=serve human_aware_rl/data/$RUN_TYPE/$RUN_NAME/seed$SEED/best human_aware_rl/data/web_models/$RUN_NAME
-
+        tensorflowjs_converter --input_format=tf_saved_model --output_node_names='ppo_agent/ppo2_model/action_probs' --saved_model_tags=serve human_aware_rl/data/$RUN_TYPE/$RUN_NAME/seed$SEED/ppo_agent human_aware_rl/data/web_models/$RUN_NAME\_seed$SEED
+    elif [ "$1" == "pbt_runs" ]
+    then
+        tensorflowjs_converter --input_format=tf_saved_model --output_node_names='agent0/ppo2_model/action_probs' --saved_model_tags=serve human_aware_rl/data/$RUN_TYPE/$RUN_NAME/seed_$SEED/agent0/best human_aware_rl/data/web_models/$RUN_NAME\_seed$SEED
+    else
+        echo "Should have 3 arguments: RUN_TYPE (ppo_runs/pbt_runs), RUN_NAME, and SEED"
+        exit 1
+    fi
+    
     cd tfjs-converter
-
-    yarn ts-node tools/pb2json_converter.ts ../human_aware_rl/data/web_models/$RUN_NAME ../human_aware_rl/data/web_models/$RUN_NAME
+    yarn ts-node tools/pb2json_converter.ts ../human_aware_rl/data/web_models/$RUN_NAME\_seed$SEED ../human_aware_rl/data/web_models/$RUN_NAME\_seed$SEED
 else
-echo "Should have 3 arguments: RUN_TYPE (ppo_runs/pbt_runs), RUN_NAME, and SEED"
-exit 1
+    echo "Should have 3 arguments: RUN_TYPE (ppo_runs/pbt_runs), RUN_NAME, and SEED"
+    exit 1
 fi
