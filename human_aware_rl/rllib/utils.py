726ae3b241bfc391eb8a5d3b5a2ec40382d11789
Bartłomiej Mielnicki <bartlomiej.mielnicki@gmail.com>
2021-02-23 23:59:50 +0100

    Utils that gets attibute of overcooked mdp/env
    They are used to get:
    - encoding functions
    - accompying gym observation spaces.

--- human_aware_rl/rllib/utils.py
+++ human_aware_rl/rllib/utils.py
@@ -1,4 +1,8 @@
 from overcooked_ai_py.agents.benchmarking import AgentEvaluator
+from overcooked_ai_py.mdp.overcooked_env import OvercookedEnv
+from overcooked_ai_py.mdp.overcooked_mdp import OvercookedGridworld
+from overcooked_ai_py.utils import only_valid_named_args
+import gym
 import numpy as np
 import inspect
 
@@ -61,3 +65,82 @@ def iterable_equal(a, b):
             return False
 
     return True
+
+
+def get_overcooked_obj_attr(attr, env=None, mdp=None, env_params=None, mdp_params=None):
+    """
+    returns overcooked object attribute based on its name; used mostly to get state processing (encoding) functions and gym spaces
+    when receives string parse it to get attribute; format is "env"/"mdp" + "." + method name i.e "env.lossless_state_encoding_mdp"
+    also support dicts (where replaces strings in values with object attributes)
+    when receives method/function returns original method; this obviously does not work this way if attr is str/dict
+    """
+    attr_type = type(attr)
+    if attr_type is str:
+        name = attr
+        [obj_name, attr_name] = name.split(".")
+        if obj_name == "mdp":
+            if not mdp:
+                if env:
+                    mdp = env.mdp
+                else:
+                    mdp = OvercookedGridworld(**mdp_params)
+            attr = getattr(mdp, attr_name)
+        elif obj_name == "env":
+            if not env:
+                if not mdp:
+                    mdp = OvercookedGridworld(**mdp_params)
+                env_params = only_valid_named_args(env_params, OvercookedEnv.from_mdp) 
+                env = OvercookedEnv.from_mdp(mdp, **env_params)
+            attr = getattr(env, attr_name)
+        # not tested or used anywhere yet
+        # elif obj_name in kwargs:
+        #     attr = getattr(kwargs[obj_name], attr_name)
+        else:
+            raise ValueError("Unsupported obj attr string "+name)
+    elif attr_type is dict:
+        attr = {k: get_overcooked_obj_attr(v, env=env, mdp=mdp, env_params=env_params, 
+            mdp_params=mdp_params) for k, v in attr.items()}
+    # not tested or used anywhere yet
+    # elif attr_type in [list, tuple]:
+    #     attr = attr_type(get_overcooked_obj_attr(elem, env=env, mdp=mdp, env_params=env_params, 
+    #         mdp_params=mdp_params) for elem in attr)
+    return attr
+
+def get_encoding_function(function, env=None, mdp=None, env_params=None, mdp_params=None):
+    """
+    returns processing function from overcooked object based on supplied name
+    when receives string parse it to get object attribute; format is "env"/"mdp" + "." + method name i.e "env.lossless_state_encoding_mdp"
+    also support dicts
+    when receives method/function returns original method; this obviously does not work this way if attr is str/dict
+    """
+    function = get_overcooked_obj_attr(function, env=env, mdp=mdp, env_params=env_params, 
+            mdp_params=mdp_params)
+
+    if type(function) is dict:
+        fn_dict = function
+        def result_fn(*args, **kwargs):
+            return {k: f(*args, **kwargs) for k, f in fn_dict.items()}
+        function = result_fn
+    # not tested or used anywhere yet
+    # elif type(function) in [list, tuple]:
+    #     fn_iterable = function
+    #     def result_fn(*args, **kwargs):
+    #         return type(function)(f(*args, **kwargs) for f in fn_iterable.items())
+    #     function = result_fn
+    return function
+
+def get_gym_space(space, env=None, mdp=None, env_params=None, mdp_params=None):
+    """
+    returns gyn observation space from overcooked object (currently only in mdps) based on supplied name
+    when receives string parse it to get object attribute; format is "env"/"mdp" + "." + method name i.e "mdp.lossless_state_encoding_gym_space"
+    also support Dict observation spaces (supply dict of strings then)
+    when receives method/function returns original method; this obviously does not work this way if attr is str/dict
+    """
+    space = get_overcooked_obj_attr(space, env=env, mdp=mdp, env_params=env_params, 
+            mdp_params=mdp_params)
+    if type(space) is dict:
+        space = gym.spaces.Dict(space)
+    # not tested or used anywhere yet
+    # elif type(space) in [list, tuple]:
+    #     space = gym.spaces.Tuple(tuple(space))
+    return space
