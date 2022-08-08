a70392909bc8e59ec4ce58278a821c471990a5c0
Alexander Lichtenstein <49325191+alexlichtenstein@users.noreply.github.com>
2022-08-08 19:09:55 +0200

    fix to install file git lfs/brew

--- install.sh
+++ install.sh
@@ -2,12 +2,15 @@
 
 # Install git-lfs for OSX
 if [[ "$OSTYPE" =~ ^darwin ]]; then
-  if command -v brew
-  then
-    brew install git-lfs
-    git lfs install
+  if hash git lfs 2>/dev/null; then
+        git lfs install
   else
-    echo "Please install brew and run the install script again"
+    if command -v brew; then
+        brew install git-lfs
+        git lfs install
+    else
+        echo "Please install brew and run the install script again"
+    fi
   fi
 fi
 
