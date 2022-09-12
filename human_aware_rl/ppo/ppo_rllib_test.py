aa834b64d534ad37903d26fc6fadf2d519c2c3ce
Alexander Lichtenstein <49325191+alexlichtenstein@users.noreply.github.com>
2022-09-13 00:39:41 +0200

    disable logging

--- human_aware_rl/ppo/ppo_rllib_test.py
+++ human_aware_rl/ppo/ppo_rllib_test.py
@@ -291,9 +291,9 @@ class TestPPORllib(unittest.TestCase):
                 "results_dir": self.temp_results_dir,
                 "num_workers": 1,
                 "num_training_iters": 1,
-                # "resume_checkpoint_path": load_path,
+                "resume_checkpoint_path": load_path,
+                "bc_model_dir": load_path,
                 "verbose": False,
-                "log_to_driver": False,
                 "evaluation_display": False
             },
             options={'--loglevel': 'ERROR'}
